2c94b1aa575424fe90973fbc688d3b8e737696a2
JJ Merelo <jjmerelo@gmail.com>
2018-07-27 18:48:53 +0200

    A bit better graph

--- draw.R
+++ draw.R
@@ -2,12 +2,12 @@ library("networkD3")
 library("igraph")
 
 data <- read_graph("perl6-ecosystem.net",format="pajek")
-issues <-  igraph_to_networkD3(data)
-sn <- sankeyNetwork(Links = issues$links, Nodes = issues$nodes, Source = "source",
+distros <-  igraph_to_networkD3(data)
+sn <- sankeyNetwork(Links = distros$links, Nodes = distros$nodes, Source = "source",
                     Target = "target", Value = "value", NodeID = "name",
                     units = "commits", fontSize = 20, nodeWidth=30)
 sn %>% saveNetwork( file= 'sankey.html' )
 
-tree <- forceNetwork(Links = issues$links, Nodes = issues$nodes, Source = "source",
-                     Target = "target", Value = "value", NodeID = "name",
+tree <- forceNetwork(Links = distros$links, Nodes = distros$nodes, Source = "source",
+                     Target = "target", Value = "value", NodeID = "name", Group="1",
                       fontSize = 20)
